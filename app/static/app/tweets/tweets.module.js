e5581accc1b65550ff18265dd9f4975287ead943
Jady Urbina <jady.urbina@upr.edu>
2017-05-19 18:18:12 -0400

    add tweet module, resolves #11

---
+++ app/static/app/tweets/tweets.module.js
@@ -0,0 +1,16 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('thsClient.tweets', []);
+        .config([
+            '$stateProvider',
+            '$urlRouterProvider',
+            Tweets
+        ]);
+
+    function Tweets($stateProvider, $urlRouterProvider) {
+
+    }
+ })();
