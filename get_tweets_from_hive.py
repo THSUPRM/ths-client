bc5b6724af850167faddeae37fb58114198d0a46
Luis Rivera <luis.rivera157@upr.edu>
2018-09-28 13:15:50 -0400

    changed hive script to get different tweets than before

--- get_tweets_from_hive.py
+++ get_tweets_from_hive.py
@@ -2,47 +2,52 @@ import sqlite3
 from datetime import datetime
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession, Row
+from pyspark.sql.functions import sort_array
 
-def getSparkSessionInstance(sparkConf):
-	if('sparkSessionInstance' not in globals()):
-		globals()['sparkSessionInstance'] = SparkSession.builder.config(conf=sparkConf).enableHiveSupport().getOrCreate()
-	return globals()['sparkSessionInstance']
+
+def get_spark_session_instance(sparkConf):
+    if 'sparkSessionInstance' not in globals():
+        globals()['sparkSessionInstance'] = SparkSession.builder.config(
+            conf=sparkConf).enableHiveSupport().getOrCreate()
+    return globals()['sparkSessionInstance']
+
 
 def create_connection(db_file):
-	try:
-		conn = sqlite3.connect(db_file)
-		return conn
-	except sqlite3.Error as e:
-		print(e)
-	return None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except sqlite3.Error as e:
+        print(e)
+    return None
 
 
 def insert_tweet(conn, tweet):
-	sql = '''INSERT OR IGNORE INTO tweets(id, text,date_created, date_modified) 
-		VALUES(?,?,?,?) '''
-	cur = conn.cursor()
-	tw = (tweet.twitter_id, tweet.full_text, datetime.today().__str__(), datetime.today().__str__())
-	try:
-		cur.execute(sql,tw)
-	except sqlite3.Error as e:
-		print(e)
+    sql = '''INSERT OR IGNORE INTO tweets(id, text,date_created, date_modified) VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    tw = (tweet.twitter_id, tweet.full_text, datetime.today().__str__(), datetime.today().__str__())
+    try:
+        cur.execute(sql, tw)
+    except sqlite3.Error as e:
+        print(e)
 
-	return cur.lastrowid
+    return cur.lastrowid
+
 
 def main():
-	database = "/home/manuelr/ths-client/app/app.db"
-	conn = create_connection(database)
-	sc = SparkContext(appName='Insert Tweets')
-	spark = getSparkSessionInstance(sc.getConf())
-	spark.sql('use thsfulltext')
-	df = spark.sql('select twitter_id, full_text from tweet limit 5000')
-	tweets = df.rdd.collect()
-	with conn:
-		for tweet in tweets:
-			insert_tweet(conn,tweet) 	
-#	conn.commit()
+    database = "/home/manuelr/ths-client/app/app.db"
+    conn = create_connection(database)
+    sc = SparkContext(appName='Insert Tweets')
+    spark = get_spark_session_instance(sc.getConf())
+    spark.sql('use thsfulltext')
+    df = spark.sql('select twitter_id, full_text from tweet')
+    tweets = df.select(sort_array(df.twitter_id)).collect().sort(key=lambda tup: tup[0])
+    i = 1
+    limit = 0
+    with conn:
+        while limit < i * 5000:
+            insert_tweet(conn, tweets[limit])
+            limit = limit + 1
+
 
 if __name__ == '__main__':
-	main()
-
-
+    main()
